c208074e95b606380ff088d6c4940abc758cdd45
Patrick Kudiatu <kudiatutomanitu@gmail.com>
2025-05-20 08:44:50 +0200

    definition u pattern  et configuration du fichier app

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,10 @@
 import "dotenv/config";
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
 
 export default app;
